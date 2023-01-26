53060feedb17eebca35ff1c883b0d372ab6244bf
Amos Maina <amossmwangii@gmail.com>
2023-01-26 04:58:42 -0800

    a program that implements the selection sort algorithm, it repeatedly selects the smallest element from the unsorted portion of the code and moves it to the sorted position

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: the first int to swap
+ * @b: the second int to swap
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of ints in ascending order using selection
+ *			sort algorithm
+ * @array: array of ints
+ * @size: array size
+ *
+ * Description: prints array after every swap
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
