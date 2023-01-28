4eaabc625af16d8e90f95c38f5e666f62b3883e5
Amos Maina <amossmwangii@gmail.com>
2023-01-28 02:55:25 -0800

    a function that implements the merge sort algorithm recursively

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,7 +3,7 @@
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 		size_t back);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
-
+void merge_sort(int *array, size_t size);
 
 /**
  * merge_subarr - sort a subarray of integers
