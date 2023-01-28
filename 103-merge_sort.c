6e814d16eb346185ee2557f289eb399d5b978391
Amos Maina <amossmwangii@gmail.com>
2023-01-28 02:53:42 -0800

    a function that implements the merge sort algorithm recursively

---
+++ 103-merge_sort.c
@@ -0,0 +1,81 @@
+#include "sort.h"
+
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+
+
+/**
+ * merge_subarr - sort a subarray of integers
+ * @subarr: sub arra of integers to sort
+ * @buff: a buffer to store the sorted subarr
+ * @front: front index of the array
+ * @mid: middle of array index
+ * @back: the back index of the array
+ */
+
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back)
+{
+	size_t i, j, k = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(subarr + mid, back - mid);
+
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
+
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j <  back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
+
+	printf("[Done}: ");
+	print_array(subarr + front, back - front);
+}
+
+/**
+ * merge_sort_recursive - implement the merge sort algo recursively
+ * @subarr: a subarray of an array of ints
+ * @buff: a buffer to store the sorted results
+ * @front: the front index of the array
+ * @back: back index of the array
+ */
+
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
+{
+	size_t mid;
+
+	if (back - front > 1)
+	{
+		mid = front + (back - front) / 2;
+		merge_sort_recursive(subarr, buff, front, mid);
+		merge_sort_recursive(subarr, buff, mid, back);
+		merge_subarr(subarr, buff, front, mid, back);
+	}
+}
+
+/**
+ * merge_sort - sorts array of integers in ascending order using
+ *		mergee sort algorithm
+ * @array: array of integers
+ * @size: array size
+ *
+ * Desc: implements the top-down merge sort algorithm
+ */
+
+void merge_sort(int *array, size_t size)
+{
+	int *buff;
+
+	if (array == NULL || size < 2)
+		return;
+
+	merge_sort_recursive(array, buff, 0, size);
+
+	free(buff);
+}
